f98f4055c75ac4598b42326dd4763c7b1b84b2dd
Ashraf <seashraf@deloitte.com>
2026-04-15 19:04:23 +0200

    created spinner component, added a new color for it, then finished the pokemon-list-inf feature and made it mobile view compatible

--- src/features/pokemon-list/components/infinite-scroll-view/InfiniteScrollView.tsx
+++ src/features/pokemon-list/components/infinite-scroll-view/InfiniteScrollView.tsx
@@ -1,9 +1,73 @@
+import { useEffect, useRef } from "react";
+import { usePokemonInfinite } from "../../hooks/usePokemonInfinite";
+import PokemonCard from "../pokemon-card/PokemonCard";
+import styles from "./InfiniteScrollView.module.scss";
+import Spinner from "@/shared/components/ui/spinner/Spinner";
 
 const InfiniteScrollView = () => {
+    const {
+        data,
+        isLoading,
+        isError,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+    } = usePokemonInfinite();
+
+    const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const target = entries[0];
+
+                if (
+                    target.isIntersecting &&
+                    hasNextPage &&
+                    !isFetchingNextPage
+                ) {
+                    fetchNextPage();
+                }
+            },
+            {
+                root: null,
+                rootMargin: "200px",
+                threshold: 0,
+            }
+        );
+
+        const el = loadMoreRef.current;
+        if (el) observer.observe(el);
+
+        return () => {
+            if (el) observer.unobserve(el);
+        };
+    }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+    if (isLoading) return <p>Loading...</p>;
+
+    if (isError) return <p>Error loading data</p>;
 
     return (
-        <div>hello infinite</div>
-    )
-}
+        <>
+            <div className={styles.cardsHolder}>
+                {data?.pages?.flatMap((page) => page.results).map((pokemon) => {
+
+                    return (
+                        <PokemonCard
+                            key={pokemon.name}
+                            name={pokemon.name}
+                            pokemonUrl={pokemon.url}
+                        />
+                    );
+                })}
+            </div>
+
+            <div ref={loadMoreRef} />
+
+            {isFetchingNextPage && <p className={styles.loadingSpan}><Spinner /> Loading more Pokemon...</p>}
+        </>
+    );
+};
 
 export default InfiniteScrollView;
---
+++ src/shared/components/ui/spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import styles from "./Spinner.module.scss";
+
+const Spinner = () => {
+    return (
+        <span className={styles.spinner} />
+    );
+}
+
+export default Spinner;
